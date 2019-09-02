0e419b1e1b43556dd09e6d3f0b38a8d75f5658a0
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2019-09-02 14:26:43 +0530

    Update razorpay-webhook.php

--- includes/razorpay-webhook.php
+++ includes/razorpay-webhook.php
@@ -40,7 +40,7 @@ class RZP_Webhook
 
 		$this->verbose	= self::VERBOSE;
 		
-		$this->timezone = self::TIMEZONE;							// MA
+		$this->timezone = self::TIMEZONE;							// MA returns 'Asia/Kolkata'
     }
 
     /**
@@ -145,168 +145,44 @@ class RZP_Webhook
 
 	/**
      * Handling the virtual acount credited webhook
-     *
+     * 
      * @param array $data Webook Data
      */
     protected function vaCredited(array $data)
     {
-		$timezone 	= new DateTimeZone($this->timezone);
+		$timezone 	= new DateTimeZone($this->timezone);	// create a new timezone object for our local time
 		
-		$payment_obj		= (object) $data['payload']['payment']['entity'];	// convert associative array to object
+		$payment_obj				= (object) $data['payload']['payment']['entity'];			// from webhook
+		$va_obj						= (object) $data['payload']['virtual_account']['entity'];	// from webhook
 		
-		$va_obj				= (object) $data['payload']['virtual_account']['entity'];
+		// get payment details based on payment ID extracted
+		$details_obj		= (object) $this->getVaPaymentEntity($payment_obj->id, $data);
 
 		$payment_datetime	= new DateTime('@' . $payment_obj->created_at);
 		$payment_datetime->setTimezone($timezone);
 
-		// get the user details based on username
-		$wp_user 			= get_user_by('login', $va_obj->notes->);
-		$wp_userid 			= $webhook_wp_user->ID ?? "web_hook_user_not_found";
+		// get the user details based on WP username that should be same as Moodle userid (not ID Number)
+		$wp_user 			= get_user_by('login', $va_obj->notes->id);		// get user by login (same as Moodle userid in User tables)
+		$wp_userid 			= $wp_user->ID ?? "web_hook_wpuser_not_found";	// get WP user ID
+		
 		// log all extracted data if verbose
-		if ($this->verbose) {
-							error_log(print_r('webhook payment_ID: ' , true));
-							error_log(print_r($razorpayPaymentId , true));
-							
-							error_log(print_r('webhook payment amount in Paise: ' , true));
-							error_log(print_r($payment_amount_p , true));
-							
-							error_log(print_r('webhook payment timestamp: ' , true));
-							error_log(print_r($data['payload']['payment']['entity']['created_at'] , true));
-							
-							error_log(print_r('webhook payment date: ' , true));
-							error_log(print_r($payment_datetime->format('Y-m-d H:i:s') , true));
-							
-							error_log(print_r('webhook payment description: ' , true));
-							error_log(print_r($payment_description , true));
-							
-							error_log(print_r('webhook payment object: ' , true));
-							error_log(print_r($va_payment , true));
-							
-							error_log(print_r('webhook payment VA ID: ' , true));
-							error_log(print_r($va_id , true));
-							
-							error_log(print_r('webhook payment sritoni ID: ' , true));
-							error_log(print_r($sritoni_id , true));
-							
-							error_log(print_r('webhook payment WP username: ' , true));
-							error_log(print_r($webhook_wp_username , true));
-							
-							error_log(print_r('webhook payment bank reference: ' , true));
-							error_log(print_r($bank_reference , true));
-							
-							error_log(print_r('webhook user WP userID ' , true));
-							error_log(print_r($webhook_wp_userid , true));
-						}
-		// get all orders not 'on-hold' with payment method va-bacs, to check for webhook payment_id already accounted for or not
-		$args = array(
-						'status' 			=> array(
-														'processing',
-														'completed',
-													),
-						'payment_method' 	=> 'vabacs',
-						'customer_id'		=> $webhook_wp_userid,
-						'meta_value'		=> $razorpayPaymentId,
-					 );
-		$orders_completed = wc_get_orders( $args ); // these are orders for this user either processing or complete
-		if (!empty($orders_completed))
-		{	// we already have completed orders that reconcile this payment ID so this webhook must be old or redundant, so quit
-			if ($this->verbose)
-			{
-				error_log(print_r('orders_completed using this payment_id:' . $razorpayPaymentId, true));
-				error_log(print_r($orders_completed, true));
-			}
-			return;
-		}
-		// end of segment added 08/21/2019---------------------------------------------
-		//
-		// Now the webhook data is fresh and so let's reconcile against open on-hold vabacs order
-		// get all orders on-hold using va-bacs payment method, for this wpuser whose ID is derived from webhook
-		// At most there should be only a few orders
-		$args = array(
-						'status' 			=> 'on-hold',
-						'payment_method' 	=> 'vabacs',
-						'customer_id'		=> $webhook_wp_userid,
-					 );
-		$orders = wc_get_orders( $args );
+		($this->verbose ? $this->logData($payment_obj, $va_obj, $details_obj, $payment_datetime, $wp_userid) : false);
 
-		if (empty($orders))
-		{	// No orders exist for this webhook payment, log that fact and exit
-			if ($this->verbose)
-			{
-				error_log(print_r('No Orders on-hold for user with sritoniID : ' . $sritoni_id .
-									'so cannot reconcile this payment_id:' . $razorpayPaymentId, true));
-			}
-			return;
-		}
+		// Is this payment already reconciled?	If so webhook redundant, exit	
+		($this->anyReconciledOrders($payment_obj->id, $wp_userid) ? return : false);
+		
+		// If we have reached this far, webhook data is fresh and so let's reconcile webhook payment against any valid open on-hold vabacs order
+		$open_orders = $this->getOpenOrders($wp_userid);
+		
+		// if null exit, there are no open orders for this user to reconcile
+		($open_orders == null ? return : false);
+		
 		// we do have open orders for this user, so lets see if we can reconcile the webhook payment to one of these open orders
-		foreach ($orders as $key => $order)
-		{
-			// get WP user id associated with order
-			$user_id = $order->get_user_id();
-			if ($this->verbose) {
-				error_log(print_r('webhook order sritoni_id:' . get_user_meta( $user_id, 'nickname', true ), true));
-				error_log(print_r('webhook order amount in paise:' . round($order->get_total() * 100), true));
-			}
-
-			// get user meta nickname holding our sritoni ID
-			if ( $sritoni_id == get_user_meta( $user_id, 'nickname', true ) )
-			{
-				// Users match. Still, do the amounts match?
-				if ( $payment_amount_p == round($order->get_total() * 100) )
-				{
-					// amounts match, is payment date after order date?
-					$order_creation_date = $order->get_date_created();
-					
-					$order_creation_datetime		= new DateTime( '@' . $order->get_date_created()->getTimestamp());
-					$order_creation_datetime->setTimezone($timezone);
-					
-					$order_number	= $order->get_order_number();
-					// is this order number contained in the payment description? check?
-
-					if ($this->verbose) {
-						error_log(print_r('payment date is after order date:' . ($payment_datetime > $order_creation_datetime), true));
-						
-						error_log(print_r('orders_number:' . $order_number, true));
-						error_log(print_r('strpos of order number in payment description:' . strpos($payment_description, $order_number), true));
-					}
-
-					if ($payment_datetime > $order_creation_datetime)
-					{
-						// we have folowing conditions satisfied for a valid match of payment to order
-						// 1. sritoni_id's match between webhood order and user's order on-hold
-						// 2. Order amounts match
-						// 3. Order is on-hold pending payment
-						// 4. Payment method is vabacs
-						// 5. Payment received only after order placed
-						// 
-						
-						$order_note = 'Payment received by Razorpay Virtual Account ID: ' . $va_id .
-					              ' Payment ID: ' . $razorpayPaymentId . '  on: ' . $payment_datetime->format('Y-m-d H:i:s') .
-					              ' Payment description: ' . $payment_description . ' bank reference: ' . $bank_reference;
-						// we found a payment that matches our condition so we add an order note so store manager can see
-						$order->add_order_note($order_note);
-						// we append the VA payment ID to the payment method of the order
-
-
-						$order->update_meta_data('va_payment_id', $razorpayPaymentId);
-						$order->update_meta_data('amount_paid_by_va_payment', $payment_amount_p * 0.01);  // in Rs
-						$order->update_meta_data('bank_reference', $bank_reference);
-						$order->update_meta_data('payment_notes_by_customer', $payment_description);
-						$order->save;
-
-						$transaction_id = $razorpayPaymentId . "," . $payment_datetime->format('Y-m-d H:i:s') . "," . $va_id .
-										"," . $bank_reference;
-						$order->payment_complete($transaction_id);
-						if ($this->verbose) {
-							error_log(print_r('Order completed and updated meta:' . $transaction_id, true));
-						}
-						break;	// we have found our order and reconciled it against webhook. No need to look further, get out of loop
-					}
-
-				}
-			}
-
-		}
+		$reconciledOrder	= $this->reconcileOrder($open_orders, $payment_obj, $va_obj, $details_obj, $payment_datetime, $wp_userid);
+		// if reconciled order is null then exit
+		($reconciledOrder 	== null ? return : false) ;
+		// Update reconciled order's meta with webhook payment details and change order status to completed
+		$this->orderUpdateMetaSetCompleted($reconciledOrder, $payment_obj, $va_obj, $details_obj, $payment_datetime, $wp_userid);
 
 	return;
 
@@ -463,4 +339,166 @@ class RZP_Webhook
 
         return (int) round($order->order_total * 100);
     }
+	
+	/**
+     * Log data extracted from webhook
+     */
+    protected function logData(($payment_obj, $va_obj, $details_obj, $payment_datetime, $wp_userid)
+    {
+        error_log(print_r('webhook payment object: ' , true));
+		error_log(print_r($payment_obj , true));
+		
+		error_log(print_r('webhook Payment Details Object: ' , true));
+		error_log(print_r($details_obj , true));
+		
+		error_log(print_r('webhook payment_ID: ' , true));
+		error_log(print_r($payment_obj->id , true));
+		
+		error_log(print_r('webhook payment amount in Paise: ' , true));
+		error_log(print_r($payment_obj->amount , true));
+		
+		error_log(print_r('webhook payment timestamp: ' , true));
+		error_log(print_r($payment_obj->created_at , true));
+		
+		error_log(print_r('webhook payment date(IST): ' , true));
+		error_log(print_r($payment_datetime->format('Y-m-d H:i:s') , true));
+		
+		error_log(print_r('webhook payment user entered description: ' , true));
+		error_log(print_r($payment_obj->description , true));
+		
+		error_log(print_r('webhook payment VA ID: ' , true));
+		error_log(print_r($va_obj->id , true));
+		
+		error_log(print_r('webhook payment sritoni ID: ' , true));
+		error_log(print_r($va_obj->notes->idnumber , true));
+		
+		error_log(print_r('webhook payment WP username: ' , true));
+		error_log(print_r($va_obj->notes->id , true));
+		
+		error_log(print_r('webhook user WP userID ' , true));
+		error_log(print_r($wp_userid , true));
+		
+		error_log(print_r('webhook payment bank reference: ' , true));
+		error_log(print_r($details_obj->bank_reference , true));
+				
+		return;
+    }
+	
+	/**
+     * Gets any orders that maybe already reconciled with this payment
+     */
+    protected function anyReconciledOrders($payment_id, $wp_userid)
+    {
+        $args = array(
+						'status' 			=> array(
+														'processing',
+														'completed',
+													),
+						'payment_method' 	=> 'vabacs',
+						'customer_id'		=> $wp_userid,
+						'meta_value'		=> $payment_id,
+					 );
+		$orders_completed = wc_get_orders( $args ); // these are orders for this user either processing or completed
+		
+		if (!empty($orders_completed))
+		{	// we already have completed orders that reconcile this payment ID so this webhook must be old or redundant, so quit
+			if ($this->verbose)
+			{
+				error_log(print_r('orders already completed using this payment_id:' . $payment_id, true));
+				error_log(print_r($orders_completed, true));
+			}
+			// true, reconciled eorders exist
+			return true;
+		}
+		// false, reconciled orders don't exist for this payment
+		return false;
+    }
+	
+	/**
+     * returns any open orders for this user
+     */
+    protected function getOpenOrders($wp_userid)
+    {
+        $args = array(
+						'status' 			=> 'on-hold',
+						'payment_method' 	=> 'vabacs',
+						'customer_id'		=> $wp_userid,
+					 );
+		$orders = wc_get_orders( $args );
+		
+		if (empty($orders))
+			{	// No orders exist for this webhook payment ID, log that fact and return null
+				if ($this->verbose)
+					{
+						error_log(print_r('No Orders on-hold for this user so cannot reconcile this webhook payment', true));
+					}
+				return null;
+			}
+		// we have valid open orders for this user		
+		return $orders;
+    }
+	
+	/**
+     * take all open orders and see if they can be reconciled against webhook payment
+	 * Order is reconciled if:
+	 * 1. Payments are same
+	 * 2. Payment date is after Order creation date
+	 * 3. Order user is same as user associated with payment: (This was already in the query for wc_get_orders)
+	 * 4. Order is on-hold: (This was already in the query for wc_get_orders)
+	 * 5. Payment method is VABACS: (This was already in the query for wc_get_orders)
+     */
+    protected function reconcileOrder($orders, $payment_obj, $va_obj, $details_obj, $payment_datetime, $wp_userid)
+    {
+        foreach ($orders as $key => $order)
+		{
+			$order_creation_datetime		= new DateTime( '@' . $order->get_date_created()->getTimestamp());
+			$order_creation_datetime->setTimezone($this->timezone);
+			
+			if 	( 
+					( $payment_obj->amount == round($order->get_total() * 100) ) 	&&		// payment amount matches order amount in paise
+					( $payment_datetime > $order_creation_datetime )				&&		// payment is after order creation
+																						)
+			{
+				// we satisfy all conditions, this order reconciles with the webhook payment
+				return $order;
+			}
+			else
+			{
+				// This order does not reconcile with our webhook payment so check for next order in loop`
+				continue;
+			}
+			
+		}
+		// we have checked all orders and none can be reconciled with our webhook payment
+		unset $order;	// because of the for each loop
+		return null;
+    }
+	/**
+     * Updates Meta of Reconciled Order and changes its status to completed
+     */
+    protected function orderUpdateMetaSetCompleted($order, $payment_obj, $va_obj, $details_obj, $payment_datetime, $wp_userid)
+    {
+		$order_note = 'Payment received by Razorpay Virtual Account ID: ' . $va_obj->id .
+					              ' Payment ID: ' . $payment_obj->id . '  on: ' . $payment_datetime->format('Y-m-d H:i:s') .
+					              ' Payment description: ' . $payment_obj->description . ' bank reference: ' . $details_obj->bank_reference;
+
+		$order->add_order_note($order_note);
+
+		$order->update_meta_data('va_payment_id', $payment_obj->id);
+		$order->update_meta_data('amount_paid_by_va_payment', $payment_obj->amount * 0.01);  // in Rs
+		$order->update_meta_data('bank_reference', $details_obj->bank_reference);
+		$order->update_meta_data('payment_notes_by_customer', $payment_obj->description);
+		$order->save;
+
+		$transaction_id = $payment_obj->id . "," . $payment_datetime->format('Y-m-d H:i:s') . "," . $va_obj->id .
+						"," . $details_obj->bank_reference;
+		$order->payment_complete($transaction_id);
+		
+		if ($this->verbose) {
+			error_log(print_r('Order completed after meta updated with Webhook payment:' . $transaction_id, true));
+		}
+		
+        return true;
+    }
+	
 }
